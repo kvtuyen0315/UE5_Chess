54e49710aee0733336a5d1cbd12b85585b616fb4
kvtuyen0315 <41115914+kvtuyen0315@users.noreply.github.com>
2023-02-19 18:31:27 +0700

    *add variable in board square.

--- Chess/Source/Chess/CppBoardSquare.cpp
+++ Chess/Source/Chess/CppBoardSquare.cpp
@@ -2,6 +2,8 @@
 
 
 #include "CppBoardSquare.h"
+#include "Components/SceneComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 ACppBoardSquare::ACppBoardSquare()
@@ -9,14 +11,20 @@ ACppBoardSquare::ACppBoardSquare()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
-	marker = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("marker"));
-	square = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("square"));
+	billboard = CreateDefaultSubobject<UBillboardComponent>(TEXT("billboard"));
 
+	marker = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("marker"));
+	marker->K2_AttachTo(billboard);
+
+	square = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("square"));
+	square->K2_AttachTo(billboard);
 }
 
 // Called when the game starts or when spawned
 void ACppBoardSquare::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	playerControllerRef = Cast<ACppPlayerController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+	gameModeRef = Cast<AChessGameModeBase>(GetWorld()->GetAuthGameMode());
 }
--- Chess/Source/Chess/CppBoardSquare.h
+++ Chess/Source/Chess/CppBoardSquare.h
@@ -4,7 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Engine/StaticMesh.h"
+#include "Components/BillboardComponent.h"
+#include "CppPlayerController.h"
+#include "ChessGameModeBase.h"
+#include "CppChessPiece.h"
+#include "Materials/MaterialInstanceBasePropertyOverrides.h"
 #include "CppBoardSquare.generated.h"
 
 UCLASS()
@@ -16,11 +20,28 @@ public:
 	// Sets default values for this actor's properties
 	ACppBoardSquare();
 
-	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent* marker;
+	UPROPERTY(EditAnywhere) UBillboardComponent* billboard;
+	UPROPERTY(EditAnywhere) UStaticMeshComponent* marker;
+	UPROPERTY(EditAnywhere) UStaticMeshComponent* square;
 
-	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent* square;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Setup") int32 x;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Setup") int32 y;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Play") bool isSlected;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Play") bool isHightlighted;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Play") bool isAttackable;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Play") bool isVisible;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "References") ACppPlayerController* playerControllerRef;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "References") AChessGameModeBase* gameModeRef;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "References") ACppChessPiece* occupantRef;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Materials") UMaterialInstance* mlHightLight;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Materials") UMaterialInstance* mlAttack;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Materials") UMaterialInstance* mlSelect;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Materials") UMaterialInstance* mlActive;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Materials") UMaterialInstance* mlLight;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Materials") UMaterialInstance* mlDark;
 
 protected:
 	// Called when the game starts or when spawned
